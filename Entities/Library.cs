d0d7eccae77bae877f729d12da02cb68eeae1dad
Danilo Marques <danilomarques289@gmail.com>
2023-07-03 20:12:29 -0300

    Update Library.cs

--- Entities/Library.cs
+++ Entities/Library.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RASCUNHO.Entities
+namespace /*Insira o seu namespace aqui*/.Entities
 {
     class Library
     {
