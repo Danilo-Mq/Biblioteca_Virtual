0a23a72250e88595ba93ccaed246f5ee54322649
Danilo Marques <danilomarques289@gmail.com>
2023-07-03 20:12:58 -0300

    Update Book.cs

--- Entities/Book.cs
+++ Entities/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RASCUNHO.Entities
+namespace /*Insira o seu namespace aqui*/.Entities
 {
     class Book
     {
@@ -29,11 +23,5 @@ namespace RASCUNHO.Entities
                 + "\n";
 
         }
-
-
-
-
-
-
     }
 }
